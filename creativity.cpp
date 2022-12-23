7e106f8fb900b88f040eca9696a14db0e3fd9758
shahmir2004 <120537360+shahmir2004@users.noreply.github.com>
2022-12-23 07:14:24 +0500

    PF week 1 uploads

---
+++ creativity.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+main() {
+
+
+
+
+
+cout<<"                                                                               " <<endl; 
+cout<<"                                                                              " <<endl; 
+cout<<"                           /\\                                                   " <<endl; 
+cout<<"                           /  \\              /\\                                   " <<endl; 
+cout<<"                          /    \\            /  \\                                     " <<endl; 
+cout<<"                         /      \\          /    \\                                   " <<endl; 
+cout<<"                        / * * * * * * * * * * * * \\                              " <<endl;
+cout<<"                        * * * * * * * * * * * * * *                                 " <<endl;
+cout<<"                       * *                       * *                                   " <<endl;
+cout<<"                      * *                         * *                               " <<endl;
+cout<<"                     * *                           * *                                 " <<endl;
+cout<<"                    * *                             * *                                  " <<endl;
+cout<<"                   * *      ---          ---         * *                        " <<endl;
+cout<<"                  * *                                * *                       " <<endl;
+cout<<"                 * *                                  * *                   " <<endl;
+cout<<"                 * *       @@@           @@@           * *                " <<endl;c
+cout<<"                 * *     @  @         @   @          * *                  " <<endl;
+cout<<"                 * *      @ ? @         @ ? @          * *                  " <<endl;
+cout<<"                 * *       @ @           @ @           * *                   " <<endl;
+cout<<"                 * *                                   * *                 " <<endl;
+cout<<"                 * *             {   }                 * *                 " <<endl;
+cout<<"                 * *            {     }                * *                 " <<endl;
+cout<<"                 * *           {       }               * *               " <<endl;
+cout<<"                 * *           (        )             * *                " <<endl;
+cout<<"                  * *      ###     -   -    ###      * *                " <<endl;
+cout<<"                   * *      ##################      * *                 " <<endl;
+cout<<"                    * *                            * *                 " <<endl;
+cout<<"                     * *                          * *                  " <<endl;
+cout<<"                       * *                       * *                   " <<endl;
+cout<<"                       * *                    * *                   " <<endl;
+cout<<"                         * *                  * *                     " <<endl;
+cout<<"                           * *               * *                     " <<endl;
+cout<<"                           ---------------------                       " <<endl;
+cout<<"                                                                                 " <<endl; 
+cout<<"                                                                                 " <<endl; 
+cout<<"                     __    ___   _____       __            ___      .    _______            " <<endl; 
+cout<<"                    |  |  |   |  |   | | /  |     |\\   |  |       /\\      |                 " <<endl;
+cout<<"                    |__|  |___|  |   | |/   |_    | \\  |  |      /  \\     |                  " <<endl;
+cout<<"                    |   | |\\    |   | |\\  |     |  \\ |  |     /____\\    |    " <<endl; 
+cout<<"                    |___| | \\   |___| | \\ |___  |   \\|  |___ /      \\   |                " <<endl; 
+
+}
---
+++ projectheader.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+main() {
+
+cout << "------------------------------------------------------------" <<endl;                          
+cout << "|                                                          |" <<endl;     
+cout << "|                     Assosiation                          |" <<endl;      
+cout << "|                                                          |" <<endl;      
+cout << "|                                                          |" <<endl;      
+cout << "|                                                          |" <<endl;      
+cout << "|                         of                               |" <<endl;      
+cout << "|                                                          |" <<endl;      
+cout << "|                                                          |" <<endl;      
+cout << "|                                                          |" <<endl;      
+cout << "|                                                          |" <<endl;      
+cout << "|                     failiure                             |" <<endl;      
+cout << "|                                                          |" <<endl;      
+cout << "|                                                          |" <<endl;      
+cout << "|                                                          |" <<endl;      
+cout << "|                                                          |" <<endl;      
+cout << "|                                                          |" <<endl;      
+cout << "------------------------------------------------------------" <<endl;                                         
+}
